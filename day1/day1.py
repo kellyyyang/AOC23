e2c52304429e22b3e3ce58586048c6a567a41cb1
kellyyyang <kelyang@hmc.edu>
2023-12-02 18:44:05 -0800

    day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -4,15 +4,11 @@ def part1(line):
     num1, num2 = '', ''
     p1_parsed = False
     p2_parsed = False
-    # print(line_length)
     while (not p1_parsed) or (not p2_parsed):
-        # print("p1, p2", line[p1], line[p2])
         if line[p1] in "1234567890" and (not p1_parsed):
-            # print("NUM1:", line[p1])
             num1 = line[p1]
             p1_parsed = True
         if line[p2] in "1234567890" and (not p2_parsed):
-            # print("NUM2:", line[p2])
             num2 = line[p2]
             p2_parsed = True
         p1 += 1
@@ -20,6 +16,49 @@ def part1(line):
     print("NUM1, NUM2", num1 + num2)
     return int(num1 + num2)
 
+def part2(line):
+    valid_nums = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+    line_length = len(line) - 1
+    p1, p2 = 0, line_length - 1
+    num1, num2 =  0, 0
+    p1_parsed = False
+    p2_parsed = False
+
+    while (not p1_parsed) or (not p2_parsed):
+        if p1 <= line_length - 3:
+            if line[p1:p1+3] in valid_nums and (not p1_parsed):
+                num1 = valid_nums[line[p1:p1+3]]
+                p1_parsed = True
+        if p2 <= line_length - 3:
+            if line[p2:p2+3] in valid_nums and (not p2_parsed):
+                num2 = valid_nums[line[p2:p2+3]]
+                p2_parsed = True
+        if p1 <= line_length - 4:
+            if line[p1:p1+4] in valid_nums and (not p1_parsed):
+                num1 = valid_nums[line[p1:p1+4]]
+                p1_parsed = True
+        if p2 <= line_length - 4:
+            if line[p2:p2+4] in valid_nums and (not p2_parsed):
+                num2 = valid_nums[line[p2:p2+4]]
+                p2_parsed = True
+        if p1 <= line_length - 5:
+            if line[p1:p1+5] in valid_nums and (not p1_parsed):
+                num1 = valid_nums[line[p1:p1+5]]
+                p1_parsed = True
+        if p2 <= line_length - 5:
+            if line[p2:p2+5] in valid_nums and (not p2_parsed):
+                num2 = valid_nums[line[p2:p2+5]]
+                p2_parsed = True
+        if line[p1] in "1234567890" and (not p1_parsed):
+            num1 = int(line[p1])
+            p1_parsed = True
+        if line[p2] in "1234567890" and (not p2_parsed):
+            num2 = int(line[p2])
+            p2_parsed = True
+        p1 += 1
+        p2 -= 1
+    print("NUM1, NUM2:", num1*10 + num2)
+    return num1*10 + num2
 
 def parse_file(filename):
     file = open(filename, 'r')
@@ -28,7 +67,6 @@ def parse_file(filename):
     running_total = 0
     for i in range(len(lines)):
         line = lines[i]
-        if i == len(lines) - 1:
-            line += ''
-        running_total += part1(line)
+        # note: added a space to the end of the last line of input.txt
+        running_total += part2(line)
     return running_total
